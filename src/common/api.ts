0fc163b7bfc1bee364e92f6e07dfb934d7c104e8
Khrulenko Vitalii <khrulenko.v@gmail.com>
2023-05-07 21:21:01 +0300

    fixed api

--- src/common/api.ts
+++ src/common/api.ts
@@ -7,7 +7,7 @@ const fetchCitiesByName = createAsyncThunk(
   async (cityName: string) => {
     try {
       const cityResponse = await fetch(
-        `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=${OPENWEATHERMAP_API_KEY}`
+        `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=${OPENWEATHERMAP_API_KEY}`
       );
 
       if (!cityResponse.ok) {
